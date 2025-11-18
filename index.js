88d895d061b852e21f84b5be28715695e3e7e186
gate1253 <gway1253@gmail.com>
2025-11-19 01:40:02 +0900

    변경사항 원복하기

--- index.js
+++ index.js
@@ -14,17 +14,8 @@ functions.http('proxy', async (req, res) => {
 
     // 헤더 필터링
     const { host, ...forwardedHeaders } = req.headers;
-    // Hop-by-hop 헤더는 프록시에서 전달하면 안 됩니다.
-    // 이를 전달하면 'protocol error'의 원인이 될 수 있습니다.
-    delete forwardedHeaders['connection'];
-    delete forwardedHeaders['keep-alive'];
-    delete forwardedHeaders['proxy-authenticate'];
-    delete forwardedHeaders['proxy-authorization'];
-    delete forwardedHeaders['te'];
-    delete forwardedHeaders['trailers'];
-    delete forwardedHeaders['transfer-encoding'];
-    delete forwardedHeaders['upgrade'];
     delete forwardedHeaders['content-length'];
+    // 304 응답을 방지하기 위해 조건부 요청 헤더 제거
     delete forwardedHeaders['if-none-match'];
     delete forwardedHeaders['if-modified-since'];
 
@@ -43,8 +34,6 @@ functions.http('proxy', async (req, res) => {
       method: req.method,
       headers,
       redirect: 'follow', // 리디렉션 따라가기
-      // 자동 압축 해제를 비활성화하여 Content-Encoding을 그대로 유지합니다.
-      compress: false,
       body,
     });
 
